aebf49501ddc682381123c38d98b40c0a28d80f6
superyang <yangchao01@163.com>
2021-06-25 09:39:22 +0800

    feat: cp_cmpo

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,3 +1,4 @@
 // export { default as Order } from './container/ParentOrderContainer'
-import Order from './container/ParentOrderContainer'
-export default Order
+// import Order from './container/ParentOrderContainer'
+// export default Order
+export { default } from './container/ParentOrderContainer'
