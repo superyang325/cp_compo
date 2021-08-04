59c57306a30232e1d22cf696daadad2aa87eb390
superyang <yangchao01@163.com>
2021-08-04 11:23:11 +0800

    feat: List

--- src/components/Order/index.js
+++
@@ -1,4 +0,0 @@
-// export { default as Order } from './container/ParentOrderContainer'
-// import Order from './container/ParentOrderContainer'
-// export default Order
-export { default } from './container/ParentOrderContainer'
--- src/constant.js
+++ src/constant.js
@@ -6,3 +6,25 @@ export const INPUTNUMBER = 'INPUTNUMBER';
 export const SELECT = 'SELECT';
 export const RADIO = 'RADIO'
 export const CHECKBOX = 'CHECKBOX'
+export const RANGEPICKER = 'RANGEPICKER'
+export const DATEPICKER = 'DATEPICKER'
+export const MONTHPICKER = 'MONTHPICKER'
+export const fieldTypes = {
+  Select:'Select',
+  Input:'Input',
+  InputNumber:'InputNumber',
+  MonthPicker:'MonthPicker',
+  DatePicker:'DatePicker',
+}
+
+export default {
+  DATERANGE,
+  INPUT,
+  INPUTNUMBER,
+  SELECT,
+  RADIO,
+  CHECKBOX,
+  RANGEPICKER,
+  DATEPICKER,
+  MONTHPICKER
+}
